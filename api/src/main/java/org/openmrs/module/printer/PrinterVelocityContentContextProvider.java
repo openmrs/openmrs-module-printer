3abb2fd8a923626ec3b4dbdd1a1bca90236f01b9
mogoodrich <mgoodrich@pih.org>
2014-09-09 13:24:24 -0400

    UHM-1560: Wristband should only be prompted for when printer set up

---
+++ api/src/main/java/org/openmrs/module/printer/PrinterVelocityContentContextProvider.java
@@ -0,0 +1,34 @@
+package org.openmrs.module.printer;
+
+import org.apache.velocity.VelocityContext;
+import org.openmrs.module.htmlformentry.FormEntrySession;
+import org.openmrs.module.htmlformentry.velocity.VelocityContextContentProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class PrinterVelocityContentContextProvider implements VelocityContextContentProvider {
+
+    @Autowired
+    private PrinterService printerService;
+
+    @Override
+    public void populateContext(FormEntrySession session, VelocityContext velocityContext) {
+
+        Map printerVelocityContext = new HashMap();
+        printerVelocityContext.put("fn", new PrinterVelocityFunctions(printerService));
+
+        // better way to do this? adding all enum values to be available in context
+        Map typeMap = new HashMap();
+        for (Printer.Type type : Printer.Type.values()) {
+            typeMap.put(type.toString(), type);
+        }
+        printerVelocityContext.put("type", typeMap);
+
+        velocityContext.put("printer", printerVelocityContext);
+    }
+
+}
