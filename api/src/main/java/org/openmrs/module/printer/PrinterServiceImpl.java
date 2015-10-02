a645bfcc8b9c8105aaf54dccfec47f8dfd2baac4
mogoodrich <mgoodrich@pih.org>
2015-10-02 10:34:18 -0400

    change threaded printing to use executor service

--- api/src/main/java/org/openmrs/module/printer/PrinterServiceImpl.java
+++ api/src/main/java/org/openmrs/module/printer/PrinterServiceImpl.java
@@ -29,11 +29,15 @@ import org.openmrs.module.printer.handler.PrintHandler;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class PrinterServiceImpl extends BaseOpenmrsService implements PrinterService {
 
@@ -282,7 +286,14 @@ public class PrinterServiceImpl extends BaseOpenmrsService implements PrinterSer
         PrintThread printThread = new PrintThread(printer, paramMap, getPrinterLock(printer.getPrinterId()), printHandler);
 
         if (printInSeparateThread) {
-            new Thread(printThread).start();
+            try {
+                ExecutorService executorService = Executors.newSingleThreadExecutor();
+                executorService.invokeAll(Arrays.asList(Executors.callable(printThread)), 2, TimeUnit.MINUTES);
+            }
+            catch (InterruptedException e) {
+                log.error("Print job interrupted for printer " + printer.getName());
+            }
+
         }
         else {
             printThread.print();
